13386e966b82f3f6877aa517f454ef12275f12f8
LukeJ <infiLuca@gmail.com>
2019-02-25 15:21:51 +0100

    update database pdo read methods

--- database-pdo/read.php
+++ database-pdo/read.php
@@ -30,3 +30,53 @@ function get_task_list() {
     return array();
   }
 }
+
+/*
+ * Select four random items/rows from the media table
+ */
+function get_random_items() {
+  include 'connection.php';
+
+  try {
+    $results = $db->query(
+      "SELECT media_id, title, category, img
+      FROM media
+      ORDER BY RAND()
+      LIMIT 4"
+    );
+  } catch (Exception $e) {
+    echo "Unable to retrieve results";
+    exit;
+  }
+}
+
+/*
+ * Pagination Function:
+ * SELECT given number of items and offset them depending on parameter
+ */
+function get_page_items($limit = null, $offset = 0) {
+  include 'connection.php';
+
+  $items_per_page = 8;
+
+  try {
+    $sql = "SELECT media_id, title, category, img
+    FROM media
+    ORDER BY title";
+
+    if (is_integer($limit)) {
+      $results = $db->prepare($sql . "LIMIT ? OFFSET ?");
+      $results->bindParam(1, $limit, PDO::PARAM_INT);
+      $results->bindParam(2, $offset, PDO::PARAM_INT);
+    } else {
+      $results = $db->prepare($sql);
+    }
+
+    $results->execute();
+  } catch (Exception $e) {
+    echo "Unable to retrieve results";
+    exit;
+  }
+
+  return $results->fetchAll();
+}
